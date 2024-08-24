dba4fd4f5714d7ad3f8c195a2af6050dfdfe11cb
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-08-24 22:07:00 +0500

    initial commit

--- lib/Views/home_screen.dart
+++ lib/Views/home_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_health_care_app_ui/utils/constants/image_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.menu, size: 32),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 12.0),
+            child: Image(
+              width: 48,
+              height: 48,
+              image: AssetImage(UImages.profile),
+            ),
+          ),
+        ],
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Choose Your Doctor',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 15),
+              Text('100+ Available', style: TextStyle(fontSize: 15)),
+
+              SizedBox(height: 30),
+
+              SearchBar(leading: Icon(Icons.search), hintText: 'Search Doctors'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,18 @@
+
+class UImages{
+  UImages._();
+
+  static const String doctor = 'assets/images/doctor.png';
+  static const String doctor1 = 'assets/images/doctor1.png';
+  static const String doctor2 = 'assets/images/doctor2.png';
+  static const String doctor3 = 'assets/images/doctor3.png';
+  static const String doctor4 = 'assets/images/doctor4.png';
+  static const String eye = 'assets/images/eye.png';
+  static const String heart = 'assets/images/heart.png';
+  static const String lungs = 'assets/images/lungs.png';
+  static const String lungs1 = 'assets/images/lungs1.png';
+  static const String menu = 'assets/images/menu.png';
+  static const String profile = 'assets/images/profile.png';
+  static const String skeleton = 'assets/images/skeleton.png';
+  static const String teeth = 'assets/images/teeth.png';
+}
